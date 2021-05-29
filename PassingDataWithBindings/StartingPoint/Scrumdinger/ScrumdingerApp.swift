1592bcbcb891baf9895b786f76c16cda09ff97ba
Uhucream <aborog1324@yahoo.co.jp>
2021-05-29 23:10:25 +0900

    Chapter.4 Passing Data with Bindings

---
+++ PassingDataWithBindings/StartingPoint/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,18 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    @State private var scrums = DailyScrum.data
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ScrumsView(scrums: $scrums)
+            }
+        }
+    }
+}
